317a97ca941b1df68f65ce3335b4cb5ad46ac24b
Hendrik Wrangel <hendrik.wrangel@init.de>
2022-09-13 10:38:41 +0200

    fork update

---
+++ packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/util/FileUtil.java
@@ -0,0 +1,50 @@
+package util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import android.content.Context;
+
+import io.flutter.Log;
+
+/**
+ * 文件工具类
+ */
+public class FileUtil {
+    private static final String JPEG_FILE_PREFIX = "IMG_";
+    private static final String JPEG_FILE_SUFFIX = ".jpg";
+
+    private static final String VIDEO_FILE_PREFIX = "VID_";
+    private static final String VIDEO_FILE_SUFFIX = ".mp4";
+
+    public static File createImageFile(Context context) throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        return image;
+    }
+
+}
